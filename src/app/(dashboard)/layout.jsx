585aa96478bb7ff68d8c411e9d3e92f9711db4cd
PAZHames <phoebehames292@gmail.com>
2024-04-05 17:32:04 +0100

    added nested layouts for dashboard and auth pages, focusing on different navbars

---
+++ src/app/(dashboard)/layout.jsx
@@ -0,0 +1,9 @@
+import Navbar from "../components/Navbar";
+
+export default function DashboardLayout({ children }) {
+  return <>
+  <Navbar/>
+  {children}
+  {/* will be page content */}
+  </>
+}
--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,10 +1,6 @@
 import { Rubik } from "next/font/google";
 import "./globals.css";
 
-// components
-import Navbar from "./components/Navbar";
-
-
 const rubik = Rubik({ subsets: ["latin"] });
 
 export const metadata = {
@@ -16,7 +12,6 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={rubik.className}>
-        <Navbar/>
           {children}
       </body>
     </html>
