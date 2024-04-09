83b6c58dd38e4271a34bc3e5863bc57959106768
PAZHames <phoebehames292@gmail.com>
2024-04-09 19:46:10 +0100

    fix: changed auth/layout.jsx (previous was dashboard), so when logged in, users cannot see the login or signup pages
    used same logic as on dashboard/layout.jsx to redirect logged in users to dashboard instead of login page

--- src/app/(auth)/layout.jsx
+++ src/app/(auth)/layout.jsx
@@ -1,7 +1,19 @@
-import React from "react";
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+
+
 import Link from "next/link";
 
-export default function AuthLayout({ children }) {
+export default async function AuthLayout({ children }) {
+
+  const supabase=createServerComponentClient({ cookies })
+  const { data } = await suapabase.auth.getSession()
+
+  if (data.session) {
+    redirect('/')
+  }
+
   return (<>
   <nav>
     <h1>Dojo Helpdesk</h1>
