333d0bb47beba2f1311ee41e0f318a42d75fbce2
PAZHames <phoebehames292@gmail.com>
2024-04-09 19:39:40 +0100

    fix: changed layout.jsx so no longer throws error if user not logged in, due to not being able to display user in navbar
    used redirect (as in server rather than client component) to redirect user to login page

--- src/app/(dashboard)/layout.jsx
+++ src/app/(dashboard)/layout.jsx
@@ -1,13 +1,20 @@
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
 // components
 import Navbar from "../components/Navbar";
+
 
 export default async function DashboardLayout({ children }) {
   const supabase=createServerComponentClient({ cookies })
   // how to get supabase in server components
   const { data } = await suapabase.auth.getSession()
+
+  if (!data.session) {
+    redirect('/login')
+    // used instead of router as this is server rather than client component 
+  }
   
   return <>
     <Navbar user= {data.session.user}/>
