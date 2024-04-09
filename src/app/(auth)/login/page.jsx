350aed79bb7f504e6c3fb301da5bb512939d19e0
PAZHames <phoebehames292@gmail.com>
2024-04-09 07:57:04 +0100

    allow users to log in again once they have logged out - use suapabase in login page, protect the route from users who are not logged in and handle for errors

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -1,17 +1,41 @@
 "use client"
 
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs"
+import { useState } from "react"
+import { useRouter } from "next/navigation"
+
+// components
 import AuthForm from "../AuthForm"
 
 export default function Login() {
 
+  const router = useRouter()
+
+  const [error,setError] = useState(``)
+
   const handleSubmit = async (e, email, password) => {
     e.preventDefault()
+    setError('')
+    // if there is an error and try to login again, removes previous error message
 
-    console.log('user login', email, password)
+    const supabase = createClientComponentClient()
+    const { error } = await supabase.auth.signInWithPassword({
+      email, 
+      password
+    })
+    if (error) {
+      setError(error.message)
+    }
+    if (!error) {
+      router.push('/')
+    }
   }
     return <main>
       <h2 className="text-center">Log in</h2>
       <AuthForm handleSubmit={handleSubmit}/>
+      {error && (
+        <div className="error">{error}</div>
+      )}
     </main>
   }
   
